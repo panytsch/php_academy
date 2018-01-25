57bc33ff1d9e52e99122212501a186111c577f4f
panytsch <romeo.crary@gmail.com>
2018-01-25 09:31:38 +0200

    to be ...

--- frontend/practice/JS_form/form_functions.js
+++ frontend/practice/JS_form/form_functions.js
@@ -1,25 +1,27 @@
-function valid(form) {
-	var name = form.my_name.value;
-	var pass = form.password.value;
-	var sex = form.sex.value;
-	var fail = false;
-	// var text = window.getElementById('text_data');
-	if (!name || name == '' || name == ' ') {
-		fail = true;
-	}
-	if (!pass || pass == '' || pass == ' ') {
-		fail = true;
-	}
-	if(!sex){
-		fail = true;
-	}
-	if (!fail) {
-		alert('Your data has been sent');
-		form.reset();
-		// text.innerHTML = 'Your data has been delivered';
-	}
-	else{
-		alert('Pls, reenter you data.\nThey\'re not correct');
-		// text.innerHTML = 'Pls, reenter you data.<br/>They\'re not correct';
+window.onload = function() {
+	function valid(form) {
+		var name = form.my_name.value;
+		var pass = form.password.value;
+		var sex = form.sex.value;
+		var fail = false;
+		var text = document.getElementById('text_data');
+		if (!name || name == '' || name == ' ') {
+			fail = true;
+		}
+		if (!pass || pass == '' || pass == ' ') {
+			fail = true;
+		}
+		if(!sex){
+			fail = true;
+		}
+		if (!fail) {
+			// alert('Your data has been sent');
+			form.reset();
+			text.innerHTML = 'Your data has been delivered';
+		}
+		else{
+			// alert('Pls, reenter you data.\nThey\'re not correct');
+			text.innerHTML = 'Pls, reenter you data.<br/>They\'re not correct';
+		}
 	}
 }
