d249e03fc92f7164e0dab368837b16e57273114f
panytsch <romeo.crary@gmail.com>
2018-01-25 23:27:44 +0200

    not yet

--- frontend/homework/008_Object/8-1_list_of_people/scr.js
+++ frontend/homework/008_Object/8-1_list_of_people/scr.js
@@ -3,7 +3,7 @@ var list = [];
 function Human(name, sname, age, sex, langs) {
 	this.name = name;
 	this.sname = sname;
-	this.age = age;
+	this.age = parseInt(age);
 	this.sex = sex;
 	this.langs = langs.split(' ');
 }
@@ -16,15 +16,33 @@ Human.prototype.show = function() {
 };
 
 function magic(form) {
-	var hum = new Human(form.name.value, form.sname.value, form.age.value, form.sex.value, form.langs.value);
-	list.push(hum);
+	if (valid(form)) {
+		var hum = new Human(form.name.value, form.sname.value, form.age.value, 
+			form.sex.value, form.langs.value);
+		list.push(hum);
+		form.reset();
+	}
+	else{
+		alert('kaka');
+	}
 }
 
 function show_people(id) {
 	var a = document.getElementById(id);
-	var str = '';
+	var str = '<tr class="train"><td>Name</td><td>Surname</td><td>Age</td><td>Sex</td><td>Programming languages</td></tr>';
 	for (var i = 0; i < list.length; i++) {
 		str += list[i].show();
 	}
 	a.innerHTML = str;
+}
+
+function valid(form) {
+	var name = form.name.value;
+	var sname = form.sname.value;
+	var age = form.age.value;
+	var sex = form.sex.value;
+	var lang = form.langs.value;
+	var flag = false;
+	!name ? flag = false : !sname ? flag = false : !age ? flag = false: isNaN(age) ? flag = false : !isFinite ? flag = false : !lang ? flag = false : !sex ? flag = false : flag = true;
+	return flag;
 }
