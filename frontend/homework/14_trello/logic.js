26eadda8594642bf55128e3aeaeb9060aa82661f
panytsch <romeo.crary@gmail.com>
2018-03-25 20:27:35 +0300

    loading..

--- frontend/homework/14_trello/logic.js
+++ frontend/homework/14_trello/logic.js
@@ -1,9 +1,24 @@
 let storage;
+let mainContent = document.getElementsByTagName('main')[0];
+let saveStorage = function() {
+    window.localStorage.mainContent = mainContent.innerHTML;
+}
+let dragenterFun = function(e) {
+    this.style.border = '3px solid red';
+}
+let dragleaveFun = function(e) {
+    this.style.border = '';
+}
 if (window.localStorage.mainContent) {
     storage = window.localStorage.mainContent;
 } else {
-    storage = '<section draggable="true"> gavno</section>';
+    storage = mainContent.innerHTML;
 }
-let mainContent = document.getElementsByTagName('main')[0];
 mainContent.innerHTML = storage;
-console.log(mainContent);
+let sections = document.getElementsByTagName('section');
+let contents = document.getElementsByClassName('content');
+for (let i = 0; contents.length > i; i++) {
+    contents[i].addEventListener('dragenter', dragenterFun, false);
+    contents[i].addEventListener('dragleave', dragleaveFun, false);
+    // contents[i].addEventListener('dragleave', , false);
+}
