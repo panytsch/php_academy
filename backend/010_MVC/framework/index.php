392d0a61b2630e9748ec75b1a8c82e719ca9400e
panytsch <romeo.crary@gmail.com>
2018-03-25 16:38:30 +0300

    i don't know what i did..

---
+++ backend/010_MVC/framework/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$name = $request->get('name', 'World');
+
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+//var_dump($response);die();
+
+$response->send();
