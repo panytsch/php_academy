567bf905351ce29120f3dba30bf670cdcf208fcd
panytsch <romeo.crary@gmail.com>
2018-03-26 18:17:41 +0300

    loading.. chekpoint

--- frontend/homework/14_trello/logic.js
+++ frontend/homework/14_trello/logic.js
@@ -5,14 +5,40 @@ let saveStorage = function() {
 }
 let dragenterFun = function(e) {
     this.style.border = '3px solid red';
-    let newElem = document.createElement('div');
-    newElem.setAttribute('class', 'content');
-    newElem.style.border = '';
-    newElem.style.backgroundColor = '#7770FF';
-    this.parentNode.insertBefore(newElem, this);
-}
+    // let newElem = document.createElement('div');
+    // newElem.setAttribute('class', 'content');
+    // newElem.setAttribute('id', 'newElement');
+    // newElem.style.border = '';
+    // this.parentNode.insertBefore(newElem, this);
+};
 let dragleaveFun = function(e) {
     this.style.border = '';
+    // this.previousSibling.remove();
+};
+let dragStartFun = function(e) {
+    this.id = 'newid';
+    // this.style.transform = 'rotate(5deg)'
+    e.dataTransfer.effectAllowed = "move";
+    this.style.border = "3px dotted #000000";
+    // console.log(e)
+    e.dataTransfer.setData("Text", this.id);
+};
+let dragendFun = function(e) {
+    this.style.border = "";
+}
+let dragoverFun = function(e) {
+    e.preventDefault();
+    return false;
+}
+let dropFun = function(e) {
+    this.style.border = '1px solid black'
+    e.preventDefault();
+    e.stopPropagation();
+    let id = e.dataTransfer.getData('Text');
+    let elem = document.getElementById(id);
+    this.parentNode.insertBefore(elem, this);
+    elem.id = '';
+    return false;
 }
 if (window.localStorage.mainContent) {
     storage = window.localStorage.mainContent;
@@ -25,5 +51,8 @@ let contents = document.getElementsByClassName('content');
 for (let i = 0; contents.length > i; i++) {
     contents[i].addEventListener('dragenter', dragenterFun, false);
     contents[i].addEventListener('dragleave', dragleaveFun, false);
-    // contents[i].addEventListener('dragleave', , false);
+    contents[i].addEventListener('dragstart', dragStartFun, false);
+    contents[i].addEventListener('dragend', dragendFun, false);
+    contents[i].addEventListener('dragover', dragoverFun, false);
+    contents[i].addEventListener('drop', dropFun, false);
 }
