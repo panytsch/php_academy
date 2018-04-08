83f54afae5f9aa78a2df2a008edd8228ca6f89d0
panytsch <romeo.crary@gmail.com>
2018-04-08 19:42:09 +0300

    all done

--- frontend/homework/14_trello/logic.js
+++ frontend/homework/14_trello/logic.js
@@ -1,7 +1,8 @@
-let main = document.getElementsByTagName('main')[0];
-let header = document.getElementsByTagName('header')[0];
-let btn = document.getElementById('button');
-let btndef = document.createElement('button');
+let main = document.getElementsByTagName('main')[0],
+    header = document.getElementsByTagName('header')[0],
+    btn = document.getElementById('button'),
+    btndef = document.createElement('button');
+dragElem = {};
 let saveAll = function() {
     let result = [];
 
@@ -35,6 +36,9 @@ btndef.onclick = function() {
     location.reload();
 }
 header.appendChild(btndef);
+if (!window.localStorage.mainContent) {
+    btndef.onclick();
+}
 let btnsave = document.createElement('button');
 btnsave.innerText = 'save';
 btnsave.type = 'button';
@@ -45,43 +49,109 @@ let addSection = function() {
     secta.render(main);
     let input = Fabric.create('input');
     input.render(secta.code);
-    saveAll();
+    // saveAll();
 }
 btn.onclick = addSection;
+
+function getCoords(elem) {
+    let box = elem.getBoundingClientRect();
+    return {
+        top: box.top + pageYOffset,
+        left: box.left + pageXOffset
+    };
+}
 
 function Fabric() {}
 Fabric.prototype.render = function(noda) {
     noda.appendChild(this.code);
 };
-Fabric.prototype.dragenterFun = function(e) {}
-Fabric.prototype.dragleaveFun = function(e) {}
+Fabric.prototype.dragFun = function(e) {
+    e.stopPropagation();
+    dragElem.clone.style.left = e.pageX - dragElem.deltaX + 'px';
+    dragElem.clone.style.top = e.pageY - dragElem.deltaY + 'px';
+    this.style.width = this.nodeName == 'DIV' ? '250px' : '280px';
+}
 Fabric.prototype.dragStartFun = function(e) {
-    this.id = 'newid';
+    e.stopPropagation();
+    let clone = this.cloneNode(true);
+    document.body.appendChild(clone);
+    clone.classList.add("clone");
+    this.id = 'newId';
+    let coords = getCoords(this),
+        deltaX = e.pageX - coords.left,
+        deltaY = e.pageY - coords.top;
+    dragElem.clone = clone;
+    dragElem.element = this;
+    dragElem.parent = this.parentNode;
+    dragElem.deltaX = deltaX;
+    dragElem.deltaY = deltaY;
+    dragElem.coordsTop = coords.top;
+    this.style.opacity = '0';
     e.dataTransfer.effectAllowed = "move";
-    e.dataTransfer.setData("Text", this.id);
+}
+Fabric.prototype.dragendFun = function(e) {
+    dragElem.element.style.opacity = '1';
+    let clone = document.querySelector('.clone');
+    let clone2 = document.querySelector('#tempId');
+    clone2.firstChild.id = '';
+    clone2.parentNode.insertBefore(clone2.firstChild, clone2);
+    clone.parentNode.removeChild(clone);
+    clone2.parentNode.removeChild(clone2);
     e.stopPropagation();
 }
-Fabric.prototype.dragendFun = function(e) {}
 Fabric.prototype.dragoverFun = function(e) {
-    e.preventDefault();
-    return false;
-}
-Fabric.prototype.dropFun = function(e) {
-    let id = e.dataTransfer.getData('Text');
-    let elem = document.getElementById(id);
-    if (this.tagName !== 'SECTION' && elem.tagName !== 'SECTION') {
-        this.parentNode.insertBefore(elem, this);
-    } else if (elem.tagName !== 'SECTION') {
-        this.insertBefore(elem, this.lastChild);
-    } else if (elem.tagName === 'SECTION' && (this.tagName === 'DIV' || this.tagName === 'input')) {
-        this.parentNode.parentNode.insertBefore(elem, this.parentNode);
-    } else {
-        this.parentNode.insertBefore(elem, this);
-    }
-    elem.id = '';
-    e.preventDefault();
     e.stopPropagation();
-    saveAll();
+    let height = parseInt(window.getComputedStyle(this, null).getPropertyValue('height'));
+    let width = parseInt(window.getComputedStyle(this, null).getPropertyValue('width'));
+    height = Math.ceil(height / 2);
+    width = Math.ceil(width / 2);
+    let delta = e.offsetY - height;
+    let delta2 = e.offsetX - width;
+    let enough = document.querySelector('#tempId') ? true : false;
+    if (!enough) {
+        let newItem = document.createElement(dragElem.element.tagName);
+        newItem.style.width = dragElem.element.tagName === 'DIV' ? (dragElem.element.offsetWidth - 14) + 'px' : dragElem.element.offsetWidth + 'px';
+        newItem.style.height = dragElem.element.offsetHeight + 'px';
+        newItem.style.backgroundColor = '#666B6B';
+        newItem.style.borderRadius = '5px';
+        newItem.id = 'tempId';
+        newItem.addEventListener('dragover', function(ev) {
+            ev.stopPropagation();
+            return;
+        }, false)
+        newItem.appendChild(document.querySelector('#newId'));
+        document.body.appendChild(newItem);
+    }
+    let newItem = document.querySelector('#tempId');
+    if (dragElem.element.tagName === 'DIV') {
+        // console.log(1);
+        if (this.tagName === 'DIV') {
+            if (delta >= 0) {
+                this.parentNode.insertBefore(newItem, this.nextSibling);
+            } else {
+                this.parentNode.insertBefore(newItem, this);
+            }
+        } else if (this.tagName === 'SECTION') {
+            this.insertBefore(newItem, this.lastChild);
+        }
+    } else if (dragElem.element.tagName === 'SECTION') {
+        if (this.tagName === 'DIV') {
+            if (delta2 < 0) {
+                this.parentNode.parentNode.insertBefore(newItem, this.parentNode);
+            } else {
+                let buff = this.parentNode.nextSibling ? true : false;
+                buff ? this.parentNode.nextSibling.parentNode.insertBefore(newItem, this.parentNode.nextSibling) : this.parentNode.parentNode.appendChild(newItem);
+            }
+        } else if (this.tagName === 'SECTION') {
+            if (delta2 < 0) {
+                this.parentNode.insertBefore(newItem, this);
+            } else {
+                let buff = this.nextSibling ? true : false;
+                buff ? this.nextSibling.parentNode.insertBefore(newItem, this.nextSibling) : this.parentNode.appendChild(newItem);
+            }
+        }
+    }
+    e.preventDefault();
     return false;
 }
 Fabric.create = function(type, text) {
@@ -95,12 +165,10 @@ Fabric.create = function(type, text) {
     }
     newFabric = new Fabric[ctor](text);
     if (newFabric.code.tagName === 'SECTION' || newFabric.code.className === 'content') {
-        newFabric.code.addEventListener('dragenter', newFabric.dragenterFun, false);
-        newFabric.code.addEventListener('dragleave', newFabric.dragleaveFun, false);
         newFabric.code.addEventListener('dragstart', newFabric.dragStartFun, false);
         newFabric.code.addEventListener('dragend', newFabric.dragendFun, false);
         newFabric.code.addEventListener('dragover', newFabric.dragoverFun, false);
-        newFabric.code.addEventListener('drop', newFabric.dropFun, false);
+        newFabric.code.addEventListener('drag', newFabric.dragFun, false);
     }
     return newFabric;
 }
@@ -144,7 +212,7 @@ Fabric.input = function() {
                 let tempor = Fabric.create('content', this.previousSibling.value);
                 this.parentNode.insertBefore(tempor.code, this.previousSibling);
                 this.previousSibling.value = '';
-                saveAll();
+                // saveAll();
             }
         }
         this.parentNode.appendChild(baton);
@@ -179,10 +247,10 @@ Fabric.section = function(text) {
     divInner.className = 'blockHeader';
     cont.appendChild(divInner);
     let baton = Fabric.create('button');
-    baton.onclick = saveAll;
+    // baton.onclick = saveAll;
     baton.render(cont);
     divInner.onfocus = function(e) {
-        this.style.width = '70%';
+        this.style.width = '60%';
         this.nextSibling.style.width = '20%';
         this.nextSibling.style.display = 'inline-block';
     }
@@ -190,6 +258,23 @@ Fabric.section = function(text) {
         this.style.width = '100%';
         this.nextSibling.style.width = '0';
         this.nextSibling.style.display = 'none';
+    }
+    diva.onmouseover = function(e) {
+        if (this.firstChild.id === 'firstId') {
+            return;
+        }
+        let newdiva = document.createElement('div');
+        newdiva.className = 'inner';
+        newdiva.id = "firstId";
+        newdiva.setAttribute('title', 'delete block');
+        newdiva.innerText = 'X';
+        newdiva.onclick = function(e) {
+            this.parentNode.parentNode.removeChild(this.parentNode);
+        }
+        this.firstChild ? this.insertBefore(newdiva, this.firstChild) : this.appendChild(newdiva);
+    }
+    diva.onmouseleave = function(e) {
+        this.removeChild(this.firstChild);
     }
     this.code.appendChild(cont);
 }
