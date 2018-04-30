a8e13b2c0c2ce662ec3f15bceb35dd8e980b5b4f
panytsch <romeo.crary@gmail.com>
2018-04-30 22:39:15 +0300

    f

---
+++ frontend/practice/React/15_React/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  render = (props) => {
+    console.log(props);
+    return (
+      <button>ckick me</button>
+    )}
+}
+
+let aFunction = () => {
+  console.log(1);
+}
+
+ReactDOM.render(
+  <App onClick={aFunction}/>,
+  document.getElementById('root')
+);
